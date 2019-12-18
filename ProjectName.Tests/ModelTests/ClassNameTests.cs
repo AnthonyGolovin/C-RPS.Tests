d059743632749f9d4b035ca835c385bca1b9a52a
Anthony Golovin and Jamila Aliyeva <anthony@>
2019-12-18 11:23:24 -0800

    practice testing.

--- ProjectName.Tests/ModelTests/ClassNameTests.cs
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -6,8 +6,13 @@ namespace ToDoList.Tests
   [TestClass]
   public class ItemTest
   {
-
-    // Test methods will go here.
+    [TestMethod]
+    public void GameResult_ReturnsString_StringReturned()
+    {
+      Game player = new Game("Rock", "Paper");
+      string result = player.GameResult("Rock" , "Paper");
+      Assert.AreEqual(result, "Paper Wins");
+    }
 
   }
 }
--- ProjectName/Models/ClassName.cs
+++ ProjectName/Models/ClassName.cs
@@ -1,9 +1,43 @@
 namespace ToDoList.Models
 {
-  public class Item
+  public class Game
   {
+    public string Player1 { get; set;}
+    public string Player2 { get; set;}
+  
+    public Game(string player1, string player2)
+    {
+      Player1 = player1;
+      Player2 = player2;
+    }
 
-    // properties, methods, etc. will go here.
-
+    public string GameResult(string player1, string player2)
+    {
+      string Paper = "Paper Wins";
+      string Rock = "Rock Wins";
+      string Scissors = "Scissors Wins";
+      string Tie = "Its a Tie";
+      
+      if(player1 == "Rock" && player2 == "Paper" || player2 == "Rock" && player1 == "Paper" )
+      {
+        return Paper;
+      }
+      if(player1 == "Paper" && player2 == "Scissors" || player2 == "Paper" && player1 == "Scissors")
+      {
+        return Scissors;
+      }
+      if(player1 == "Rock" && player2 == "Scissors" || player1 == "Scissors" && player2 == "Rock")
+      {
+        return Rock;
+      }
+      if(Player1 == Player2)
+      {
+        return Tie;
+      }
+      else 
+      {
+        return "";
+      }
+    }
   }
 }
